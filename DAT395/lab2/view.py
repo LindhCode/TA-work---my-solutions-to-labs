c7a62660a5594a53a4dffc0490f6b4c861019a37
axellindh2004@gmail.com <axellindh2004@gmail.com>
2026-08-30 14:30:51 +0200

    Task 8/12 complete

--- DAT395/lab2/view.py
+++ DAT395/lab2/view.py
@@ -1,6 +1,24 @@
+from tkinter import *
+from model import *
+
+root = Tk()
+
+canvas = Canvas(root, bg="white", width=800, height=600)
+canvas.pack()
+
+o = canvas.create_oval(80, 30, 140, 150, fill="green")
+
 # Task (7/12): Draw on canvas
+input()
 
 # Task (8/12): Define a new function to_canvas_coords(canvas, x)
+
+def to_canvas_coords(canvas, u: Vec):
+    h = canvas.winfo_reqheight()
+    w = canvas.winfo_reqwidth()
+    x_canvas = (h/20) * u.x + w/2
+    y_canvas = (h/20) * -u.y + h/2
+    return Vec(x_canvas, y_canvas)
 
 #######################################
 ### NB. Task 9 is done in model.py. ###
